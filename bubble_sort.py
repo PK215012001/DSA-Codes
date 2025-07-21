38b2888b44b71495e0fc1fd63e01a1a0ab3ef23f
PK215012001 <gpavankalyanreddy0@gmail.com>
2025-07-21 07:23:15 +0000

    Modified some code for sorting techniques

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,12 +1,12 @@
 # sorting an array happens by pushing the maximum to last by adjacent swappings
 # swap count will make it optimised for bubble sort
 num_list = [25, 87, 102, 15, 2, 32]
-for i in range(len(num_list)-1):
+for i in range(len(num_list), 1, -1):
     swap_count = 0
-    for j in range(len(num_list) - i - 1):
+    for j in range(i - 1):
         if (num_list[j] > num_list[j+1]):
             num_list[j], num_list[j+1] = num_list[j+1], num_list[j]
             swap_count = 1
     if (swap_count == 0):
         break
-print(num_list) 
+print(num_list)
